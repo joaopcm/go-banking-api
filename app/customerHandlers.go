b8013bfa11aebd863def91cb896c511f0c21fb5b
João Melo <jopcmelo@gmail.com>
2022-02-01 20:23:50 -0300

    refactor: improve error handling

--- app/customerHandlers.go
+++ app/customerHandlers.go
@@ -3,7 +3,6 @@ package app
 import (
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -39,10 +38,17 @@ func (ch *CustomerHandlers) getCustomer(w http.ResponseWriter, r *http.Request) 
 	customer, err := ch.service.GetCustomer(id)
 
 	if err != nil {
-		w.WriteHeader(err.Code)
-		fmt.Fprint(w, err.Message)
+		writeResponse(w, err.Code, err.AsMessage())
 	} else {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customer)
+		writeResponse(w, http.StatusOK, customer)
+	}
+}
+
+func writeResponse(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		panic(err)
 	}
 }
--- errs/errors.go
+++ errs/errors.go
@@ -3,8 +3,14 @@ package errs
 import "net/http"
 
 type AppError struct {
-	Code int
-	Message string
+	Code int `json:",omitempty"`
+	Message string `json:"message"`
+}
+
+func (e AppError) AsMessage() *AppError {
+	return &AppError{
+		Message: e.Message,
+	}
 }
 
 func NewNotFoundError(message string) *AppError {
