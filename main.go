c30e95b2bfe0a9c89aeca85ee687ca7f2693ebad
João Melo <jopcmelo@gmail.com>
2022-02-01 08:53:56 -0300

    feat: add XML encoding

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name string `json:"full_name"`
-	City string `json:"city"`
-	Zipcode string `json:"zip_code"`
+	Name string `json:"full_name" xml:"name"`
+	City string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zipcode"`
 }
 
 func main() {
@@ -30,7 +31,11 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		{Name: "John Doe", City: "New York", Zipcode: "00000000"},
 	}
 
-	w.Header().Add("Content-Type", "application/json")
-
-	json.NewEncoder(w).Encode(customers)
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
 }
